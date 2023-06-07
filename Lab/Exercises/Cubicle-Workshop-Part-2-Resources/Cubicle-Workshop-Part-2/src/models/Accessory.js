520dc35fe0a378a32b03760eb30a90fd5575b81c
Kristiqn Stoykow <kristiqn.stoikow11@gmail.com>
2023-06-07 18:39:14 +0300

    Adding Accessory Model and connecting it to Cube model

---
+++ Lab/Exercises/Cubicle-Workshop-Part-2-Resources/Cubicle-Workshop-Part-2/src/models/Accessory.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const AccessorySchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    imageUrl: {
+        type: String, required: true, match: /^https?:\/\//
+    },
+    description: { type: String, required: true, maxLength: 50 },
+});
+
+const Accessory = mongoose.model('Accessory', AccessorySchema);
+
+module.exports = Accessory;
--- Lab/Exercises/Cubicle-Workshop-Part-2-Resources/Cubicle-Workshop-Part-2/src/models/Cube.js
+++ Lab/Exercises/Cubicle-Workshop-Part-2-Resources/Cubicle-Workshop-Part-2/src/models/Cube.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 
 const CubeSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    description: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    difficulty: { type: Number, required: true, min: 1, max: 6 }
+    description: { type: String, required: true, maxLength: 50 },
+    imageUrl: {
+        type: String, required: true, match: /^https?:\/\//
+    },
+    difficulty: { type: Number, required: true, min: 1, max: 6 },
+    accessories: [{ type: mongoose.Types.ObjectId, ref: 'Accessory' }],
 });
 
 const Cube = mongoose.model('Cube', CubeSchema);
