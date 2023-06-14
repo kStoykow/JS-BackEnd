40bb94597360e5704ca3e3b730d15a974cb0ab5d
Kristiqn Stoykow <kristiqn.stoikow11@gmail.com>
2023-06-14 12:25:42 +0300

    Removing scaffold

--- Lab/Exam-prep/scaffold/controllers/details.js
+++
@@ -1,7 +0,0 @@
-const details = require('express').Router();
-
-details.get('/:id', (req, res) => {
-    res.render('details');
-});
-
-module.exports = details;
--- Lab/Exam-prep/scaffold/controllers/home.js
+++
@@ -1,8 +0,0 @@
-const home = require('express').Router();
-
-home.get('/', (req, res) => {
-    console.log(req.user);
-    res.render('home');
-});
-
-module.exports = home;
